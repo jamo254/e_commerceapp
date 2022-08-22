a701fa18e30e7e57e02c5713aee5d58e53cecfc2
Fred Denis <fredjamo68@gmail.com>
2022-08-22 12:32:08 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:e_commerceapp/pages/Homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+      routes: {
+         "/": ((context) => HomePage())
+      },
+    );
+  }
+}
---
+++ lib/pages/Homepage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/HomeAppBar.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(children: [
+         HomeAppBar(),
+      ]),
+    );
+  }
+}
